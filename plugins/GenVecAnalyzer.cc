25162148768df7acfb63f87838f3ad5d1e04c7cd
Kevin Pedro <kpedro88@gmail.com>
2022-10-12 15:33:34 -0500

    fix bug in finding mediator decay products

--- plugins/GenVecAnalyzer.cc
+++ plugins/GenVecAnalyzer.cc
@@ -320,9 +320,11 @@ void GenVecAnalyzer::firstDark(CandPtr part, CandSet& firstMd, CandPtr& firstQdM
 			for(unsigned i = 0; i < part->numberOfDaughters(); i++){
 				CandPtr dau = part->daughter(i);
 				if (isParticle(DarkTMediatorIDs_,dau)){
-					firstMd.insert(dau);
-					// once a mediator daughter is found, we look for the descendants of the mediator
-					medDecay(dau,firstQdM1,firstQdM2,firstQsM1,firstQsM2,secondDM,secondSM);
+					if(firstMd.find(dau)==firstMd.end()){
+						firstMd.insert(dau);
+						// once a mediator daughter is found, we look for the descendants of the mediator
+						medDecay(dau,firstQdM1,firstQdM2,firstQsM1,firstQsM2,secondDM,secondSM);
+					}
 				}
 			}
 		}
