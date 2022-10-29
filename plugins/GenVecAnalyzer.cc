83df7d2bc58d0f0d4312104f22593c145ed9b229
Kevin Pedro <kpedro88@gmail.com>
2022-10-29 11:27:14 -0500

    add Meff

--- plugins/GenVecAnalyzer.cc
+++ plugins/GenVecAnalyzer.cc
@@ -103,6 +103,7 @@ class GenVecAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources,edm
 			double MTx;
 			double MT2;
 			double MAOS;
+			double Meff;
 			LorentzVector Met1;
 			LorentzVector Met2;
 			double PairMT2;
@@ -221,6 +222,7 @@ void GenVecAnalyzer::beginJob()
 	tree->Branch("MTx", &entry.MTx, "MTx/D");
 	tree->Branch("MT2", &entry.MT2, "MT2/D");
 	tree->Branch("MAOS", &entry.MAOS, "MAOS/D");
+	tree->Branch("Meff", &entry.Meff, "Meff/D");
 	tree->Branch("Met1", "Met1", &entry.Met1, 32000, 99);
 	tree->Branch("Met2", "Met2", &entry.Met2, 32000, 99);
 	tree->Branch("PairMT2", &entry.PairMT2, "PairMT2/D");
@@ -489,6 +491,7 @@ void GenVecAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
 	entry.MT = TransverseMass(vjj.px(),vjj.py(),vjj.mass(),entry.Met.px(),entry.Met.py(),0.0);
 	//for boosted case
 	entry.MTx = TransverseMass(entry.Jet2.px(),entry.Jet2.py(),entry.Jet2.mass(),entry.Met.px(),entry.Met.py(),0.0);
+	entry.Meff = entry.Jet2.pt()+entry.Met.pt();
 
 	//compute MT2 and MAOS
 	double METx = i_met.px();
